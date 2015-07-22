cc73cca7b5a95e931713292002ac658b1c4690c3
Martino Dell'Ambrogio <martino.dellambrogio@kyos.ch>
2015-07-22 10:45:19 +0200

    v1.1

--- dyndaemon.pl
+++ dyndaemon.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# dyndaemon.pl v1.0
+# dyndaemon.pl
 #
 # Martino Dell'Ambrogio <tillo@httpdnet.com>, 2006, GPLv2
 
@@ -14,6 +14,8 @@ $IFACE=$options{i} or $IFACE="ppp0";
 $DEBUG=$options{d} or $DEBUG="0";
 $DONTDOANYTHING=1 if defined $options{n};
 
+$CHKHIDDENFIELDSEPARATOR = '#';
+
 open(CONFHANDLE, "/etc/dyndaemon.conf") or open(CONFHANDLE, "/usr/local/etc/dyndaemon.conf") or open(CONFHANDLE, "dyndaemon.conf") or die "Can't open config file!";
 
 $BLOCK=0;
@@ -23,7 +25,7 @@ $NEWIPP=~s/\./\\\./g;
 
 print "[D1] New IP is \"".$NEWIP."\".\n" if $DEBUG>=1;
 
-print "[D3] New IP parsed is \"".$NEWIPP."\".\n" if $DEBUG>=3;
+print "[D3] New IP to be parsed is \"".$NEWIPP."\".\n" if $DEBUG>=3;
 
 while(<CONFHANDLE>) {
 
@@ -45,13 +47,20 @@ while(<CONFHANDLE>) {
     $SUCCESS=0;
     
   }
-  elsif ($_ =~ /^chk (.*?) (.*)$/ && $BLOCK == 1) {
+  elsif ($_ =~ /^chk (.+?) (.+)$/ && $BLOCK == 1) {
 
     $FILENAME=$1;
     $STRING=$2;
     print "[D1] Checking \"".$FILENAME."\" for string \"".$STRING."\".\n" if $DEBUG>=1;
 
-    $STRINGPO=$STRINGS=$STRINGP=$2;
+    if ($2 =~ /^(.+)$CHKHIDDENFIELDSEPARATOR(.+)$/) {
+      print "[D3] chk's tirdh hidden field encountered.\n" if $DEBUG>=3;
+      $STRINGPO=$STRINGP=$1;
+      $STRINGS=$2;
+    }
+    elsif ($2 =~ /^([^$CHKHIDDENFIELDSEPARATOR]+)$/) {
+      $STRINGPO=$STRINGS=$STRINGP=$1;
+    }
 
     $STRINGP=~s/\{\}/\[0-9\]\{1,3\}\\\.\[0-9\]\{1,3\}\\\.\[0-9\]\{1,3\}\\\.\[0-9\]\{1,3\}/;
     print "[D3] Regexp to parse is \"".$STRINGP."\".\n" if $DEBUG>=3;
@@ -59,7 +68,7 @@ while(<CONFHANDLE>) {
     $STRINGS=~s/\{\}/$NEWIP/;
     print "[D3] Substitute is \"".$STRINGS."\".\n" if $DEBUG>=3;
 
-    tie @filelines, 'Tie::File', $FILENAME or die "Can't open file!";
+    tie @filelines, 'Tie::File', $FILENAME or die "Can't open file! ($FILENAME)";
     print "[D2] File opened.\n" if $DEBUG>=2;
 
     $i=1;
@@ -71,7 +80,7 @@ while(<CONFHANDLE>) {
 
         print "[D2] Found string \"".$_."\" at line ".$i.".\n" if $DEBUG>=2;
 
-        $STRINGPO=~s/\{\}/\(\.\*\)/;
+        $STRINGPO=~s/\{\}/\(\.\+\)/;
         print "[D3] Regexp to parse for old IP is \"".$STRINGPO."\".\n" if $DEBUG>=3;
 
         $_=~/$STRINGPO/;
@@ -99,7 +108,7 @@ while(<CONFHANDLE>) {
     untie @filelines;
     
   }
-  elsif ($_ =~ /^exe (.*)$/ && $BLOCK == 1 && $SUCCESS == 1) {
+  elsif ($_ =~ /^exe (.+)$/ && $BLOCK == 1 && $SUCCESS == 1) {
 
     print "[D1] Executing \"".$1."\"\n" if $DEBUG>=1;
     
@@ -112,3 +121,4 @@ while(<CONFHANDLE>) {
 close(CONFHANDLE);
 
 exit 0;
+
